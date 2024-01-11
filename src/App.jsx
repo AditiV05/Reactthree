14232734f5637117d406723872dda82414d94fdd
aditikalvium <145761082+aditikalvium@users.noreply.github.com>
2024-01-11 15:51:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"; // You may have this import if you want to use styles
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Kalvium</h1>
+    </div>
+  );
+}
+
+export default App;
